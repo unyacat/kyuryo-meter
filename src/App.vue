7ed576e25b80af699e6bef7f863220575af3fed6
unyacat <admin@unyacat.net>
2021-03-17 16:10:01 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <header>
+      <span>給料メーター</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <kyuryo></kyuryo>
+    </main>
+  </div>
+</template>
+
+<script>
+import Kyuryo from './components/kyuryo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Kyuryo
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ src/components/Kyuryo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div id="kyuryo-main">
+      {{ earnedMoney }}
+      <span id="kyuryo-sub">
+        .{{ earn }}
+      </span>
+      円
+    </div>
+    <button @click='calcEarnedMoney'></button>
+  </div>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+
+export default {
+  data() {
+    return {
+      earn: 3853
+    }
+  },
+  methods: {
+    calcEarnedMoney: function() {
+      const from = dayjs('2021-03-16 21:00')
+      let now = dayjs()
+      let res = now - from
+      this.earn = res
+    }
+  },
+  mounted() {
+    setInterval(this.calcEarnedMoney, 1)
+  },
+  computed: {
+    earnedMoney: function() {
+      let res = this.earn
+      return res
+    }
+  }
+}
+</script>
+
+<style>
+#kyuryo-main {
+  font-size: 84px;
+}
+
+#kyuryo-sub {
+  font-size : 32px;
+}
+</style>
