45133843cfe88f4388a6e34b5519781f66772ef4
unyacat <admin@unyacat.net>
2021-03-20 22:58:12 +0900

    Affiliate

---
+++ src/components/AmazonAffiliate.vue
@@ -0,0 +1,13 @@
+<template>
+ <iframe src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=13&l=ez&f=ifr&linkID=b575064ae015ad82e5bd6886ec15ae8c&t=unyacat03-22&tracking_id=unyacat03-22" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'AmazonAffiliate'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/RakutenAffiliate.vue
@@ -0,0 +1,13 @@
+<template>
+   <a href="https://hb.afl.rakuten.co.jp/hsc/1f652be1.a91ad32f.1770aff7.eaa3fb55/?link_type=pict&ut=eyJwYWdlIjoic2hvcCIsInR5cGUiOiJwaWN0IiwiY29sIjoxLCJjYXQiOiI0NCIsImJhbiI6IjQ2MDEzNSIsImFtcCI6ZmFsc2V9" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hsb/1f652be1.a91ad32f.1770aff7.eaa3fb55/?me_id=1&me_adv_id=460135&t=pict" border="0" style="margin:2px" alt="" title=""></a>
+</template>
+
+<script>
+export default {
+   name: 'RakutenAffiliate'
+}
+</script>
+
+<style>
+
+</style>
