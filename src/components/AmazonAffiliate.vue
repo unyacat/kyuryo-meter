b21db00b7e2ac343178f347ce72394f52bce8b76
unyacat <admin@unyacat.net>
2021-03-21 00:41:46 +0900

    Responseive Ads

--- src/components/AmazonAffiliate.vue
+++ src/components/AmazonAffiliate.vue
@@ -1,5 +1,7 @@
 <template>
- <iframe src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=13&l=ez&f=ifr&linkID=b575064ae015ad82e5bd6886ec15ae8c&t=unyacat03-22&tracking_id=unyacat03-22" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+  <div class="iframe-parent">
+    <iframe src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=13&l=ez&f=ifr&linkID=b575064ae015ad82e5bd6886ec15ae8c&t=unyacat03-22&tracking_id=unyacat03-22" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,16 @@ export default {
 </script>
 
 <style>
-
+.iframe-parent {
+   position: relative;
+   width: 100%;
+   padding-top: 12.8%;
+}
+.iframe-parent iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
 </style>
--- src/components/RakutenAffiliate.vue
+++ src/components/RakutenAffiliate.vue
@@ -1,5 +1,7 @@
 <template>
-   <a href="https://hb.afl.rakuten.co.jp/hsc/1f652be1.a91ad32f.1770aff7.eaa3fb55/?link_type=pict&ut=eyJwYWdlIjoic2hvcCIsInR5cGUiOiJwaWN0IiwiY29sIjoxLCJjYXQiOiI0NCIsImJhbiI6IjQ2MDEzNSIsImFtcCI6ZmFsc2V9" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hsb/1f652be1.a91ad32f.1770aff7.eaa3fb55/?me_id=1&me_adv_id=460135&t=pict" border="0" style="margin:2px" alt="" title=""></a>
+   <div class="resizeimage">
+      <a href="https://hb.afl.rakuten.co.jp/hsc/1f652be1.a91ad32f.1770aff7.eaa3fb55/?link_type=pict&ut=eyJwYWdlIjoic2hvcCIsInR5cGUiOiJwaWN0IiwiY29sIjoxLCJjYXQiOiI0NCIsImJhbiI6IjQ2MDEzNSIsImFtcCI6ZmFsc2V9" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hsb/1f652be1.a91ad32f.1770aff7.eaa3fb55/?me_id=1&me_adv_id=460135&t=pict" border="0" style="margin:2px" alt="" title=""></a>
+   </div>
 </template>
 
 <script>
@@ -9,5 +11,7 @@ export default {
 </script>
 
 <style>
-
+.resizeimage img {
+   width: 100%;
+}
 </style>
